78202c05f24b38e2321ecd74bc80eb30e735b558
Melanie Sifen <melanie.sifen@gmail.com>
2019-10-25 13:37:43 -0500

    added unit tests

--- TestDiplomacy.py
+++ TestDiplomacy.py
@@ -15,144 +15,72 @@
 from io import StringIO
 from unittest import main, TestCase
 
-from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
+from Diplomacy import diplomacy_solve
 
 # -----------
-# TestCollatz
+# TestDiplomacy
 # -----------
 
 
-class TestCollatz (TestCase):
+class TestDiplomacy (TestCase):
     # ----
     # read
     # ----
 
-    def test_read(self):
-        s = "1 10\n"
-        i, j = collatz_read(s)
-        self.assertEqual(i,  1)
-        self.assertEqual(j, 10)
-    # switched order
-
-    def test_read2(self):
-        s = "10 1\n"
-        i, j = collatz_read(s)
-        self.assertEqual(i,  10)
-        self.assertEqual(j, 1)
-    # floats interpreted as ints
-
-    def test_read3(self):
-        s = "1.0 10.0\n"
-        i, j = collatz_read(s)
-        self.assertEqual(i,  1)
-        self.assertEqual(j, 10)
-
-    # ----
-    # eval
-    # ----
-
-    def test_eval_1(self):
-        v = collatz_eval(1, 10)
-        self.assertEqual(v, 20)
-
-    def test_eval_2(self):
-        v = collatz_eval(100, 200)
-        self.assertEqual(v, 125)
-
-    def test_eval_3(self):
-        v = collatz_eval(1, 1000)
-        self.assertEqual(v, 179)
-
-    def test_eval_4(self):
-        v = collatz_eval(1001, 3005)
-        self.assertEqual(v, 217)
-
-    def test_eval_5(self):
-        v = collatz_eval(100000, 9000)
-        self.assertEqual(v, 351)
-
-    def test_eval_6(self):
-        v = collatz_eval(10, 10)
-        self.assertEqual(v, 7)
-
-    # -----
-    # print
-    # -----
-
-    def test_print(self):
-        w = StringIO()
-        collatz_print(w, 1, 10, 20)
-        self.assertEqual(w.getvalue(), "1 10 20\n")
-
-    def test_print2(self):
-        w = StringIO()
-        collatz_print(w, 9000, 100000, 351)
-        self.assertEqual(w.getvalue(), "9000 100000 351\n")
-
-    def test_print3(self):
-        w = StringIO()
-        collatz_print(w, 0, 10, 0)
-        self.assertEqual(w.getvalue(), "0 10 0\n")
-
-    # -----
-    # solve
-    # -----
-
     def test_solve(self):
-        r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
+        r = StringIO("A Madrid Hold\n")
         w = StringIO()
-        collatz_solve(r, w)
+        diplomacy_solve(r, w)
         self.assertEqual(
-            w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
-    # testing floats
+            w.getvalue(), "A Madrid\n")
+    
 
     def test_solve2(self):
-        r = StringIO("1 10.0\n100.0 200\n201 210.0\n900.0 1000.0\n")
+        r = StringIO("A Madrid Hold\n B Barcelona Move Madrid\n C London Support B\n")
         w = StringIO()
-        collatz_solve(r, w)
+        diplomacy_solve(r, w)
         self.assertEqual(
-            w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
-    # testing reversed i, j
+            w.getvalue(), "A [dead]\n B Mardid\n C London\n")
+    
 
     def test_solve3(self):
-        r = StringIO("10 1\n")
+        r = StringIO("A Madrid Hold\n B Barcelona Move Madrid\n")
         w = StringIO()
-        collatz_solve(r, w)
+        diplomacy_solve(r, w)
         self.assertEqual(
-            w.getvalue(), "10 1 20\n")
+            w.getvalue(), "A [dead]\n B [dead]\n")
+        
+    def test_solve4(self):
+        r = StringIO("A Madrid Hold\n B Barcelona Move Madrid\n C London Support B\n D Austin Move London\n")
+        w = StringIO()
+        diplomacy_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "A [dead]\n B [dead]\n C [dead]\n D [dead]\n")
+        
+    def test_solve4(self):
+        r = StringIO("A Madrid Hold\n B Barcelona Move Madrid\n C London Move Madrid\n")
+        w = StringIO()
+        diplomacy_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "A [dead]\n B [dead]\n C [dead]\n")
+        
+    def test_solve5(self):
+        r = StringIO("A Madrid Hold\n B Barcelona Move Madrid\n C London Support B\n D Paris Support B\n")
+        w = StringIO()
+        diplomacy_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "A [dead]\n B Madrid\n C [dead]\n D Paris\n")
+        
+    def test_solve6(self):
+        r = StringIO("A Madrid Hold\n B Barcelona Move Madrid\n C London Move Madrid\n D Paris Support B\n E Austin Support A\n")
+        w = StringIO()
+        diplomacy_solve(r, w)
+        self.assertEqual(
+            w.getvalue(), "A [dead]\n B [dead]\n C [dead]\n D Paris\n E Austin\n")
 
 # ----
 # main
 # ----
 
-
 if __name__ == "__main__":
     main()
-
-""" #pragma: no cover
-% coverage run --branch TestCollatz.py >  TestCollatz.out 2>&1
-
-
-% cat TestCollatz.out
-.......
-----------------------------------------------------------------------
-Ran 7 tests in 0.000s
-OK
-
-
-% coverage report -m                   >> TestCollatz.out
-
-
-
-% cat TestCollatz.out
-.......
-----------------------------------------------------------------------
-Ran 7 tests in 0.000s
-OK
-Name             Stmts   Miss Branch BrPart  Cover   Missing
-------------------------------------------------------------
-Collatz.py          12      0      2      0   100%
-TestCollatz.py      32      0      0      0   100%
-------------------------------------------------------------
-TOTAL               44      0      2      0   100%
-"""
