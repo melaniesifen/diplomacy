fa4caa0f062e35f4555608408fa6645d4c38ac6b
optimuhs <franciscoj12m.@hotmail.com>
2019-10-31 15:20:07 -0500

    Cleaned up and semi changed the file. Let me know what you think

--- Diplomacy2.py
+++ Diplomacy2.py
@@ -26,7 +26,7 @@ class War(Army):
         else:
     
     #Move and track 
-    def cityTracker(self):
+    def cityTracker(self, city_lst):
         #Dictionary to hold cities and armies occupying the cities
         #{city : [List of armies in city],...}
         city_dict = {}
@@ -39,7 +39,7 @@ class War(Army):
 
     # packs the two parts of the action (if there are two parts) into one item
     # returns the army, city, and action      
-    def diplomacy(self, s):
+    def diplomacyZip(self, s):
         s2 = iter(s) # make s into an iterator
         army, city, action = next(s2), next(s2), list(s2)
         return army, city, action
@@ -47,105 +47,15 @@ class War(Army):
 
         
 # read std in
-def diplomacy_start(r, w):
+def diplomacy_read(r, w):
     r = r.split("\n") # splits reader by line
-    lst = []
     dip = War()
+    lst = []
     for s in r: # for string in reader
-        army, city, action = diplomacy(s)
+        army, city, action = dip.diplomacyZip(s)
         #Create Army objects
         army = Army(action, location = city)
         lst.append(army)
+    dip.cityTracker(lst)
+    return dip 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# read std in and solve warfare problem
-def diplomacy_start(r, w):
-    r = r.split("\n") # splits reader by line
-    lst = []
-    for s in r: # for string in reader
-        army, city, action = diplomacy_read(s)
-        new_city = diplomacy_action(action, city) # new city is determined by the action
-        lst += (army, new_city, action)
-    diplomacy_eval(lst)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def diplomacy_eval(lst):
-    # if cities are all different, all armies are alive
-    city_lst = list(zip(*lst))[1] # list of all cities
-    if len(city_lst) == len(set(city_lst)): # checks for unique cities
-        return #army city, army ciy, ... army, city
-    # if city is repeated, then multiple armies are in one city (at war)
-    armies_at_war = diplomacy_warfare(city_lst, lst)
-    # for item in lst: largest count = city, others = [dead]
-    
-
-# create new list of cities with duplicates
-# return the list of armies at war with support
-def diplomacy_warfare(city_lst, lst):
-    city_lst_duplicates1, city_lst_duplicates2 = set(), set()
-    add1, add2 = city_lst_duplicates1.add, city_lst_duplicates2.add
-    for city in city_lst:
-        if city in city_lst_duplicates1:
-            add2(city)
-        else:
-            add1(city)
-    cities_at_war = list(city_lst_duplicates2) # list of all duplicate cities (cities at war)
-    
-    armies_at_war = []
-    for city in cities_at_war:
-        armies = []
-        for tup in lst:
-            if tup[1] == city:
-                armies += tup[0]
-        armies_at_war += [armies]
-    #Dictionary for 
